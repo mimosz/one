afe990fa60f0a646a2fdc7b95215dacf226644bf
Howl王 <mimosa@shareday.com>
2012-08-13 09:48:28 +0800

    修改任务名称

--- lib/resque_init.rb
+++ lib/resque_init.rb
@@ -16,7 +16,7 @@ User.all.each do |user|
   Resque.set_schedule("items-#{user.user_id}", { every: '2h', queue: :items, class: 'ResqueJobs::SyncItem', args: user.id })
   Resque.set_schedule("trades-#{user.user_id}", { every: '1h', queue: :trades, class: 'ResqueJobs::SyncTrade', args: user.id })
   Resque.set_schedule("base-#{user.user_id}", { cron: '0 16 * * *', queue: :base, class: 'ResqueJobs::SyncShipping', args: user.id })
-  Resque.set_schedule("base-#{user.user_id}", { cron: '0 11 * * *', queue: :base, class: 'ResqueJobs::SyncShipping', args: user.id })
+  Resque.set_schedule("shippings-#{user.user_id}", { cron: '0 11 * * *', queue: :shippings, class: 'ResqueJobs::SyncShipping', args: user.id })
 end
 # 清除任务
 User.all.each do |user|
