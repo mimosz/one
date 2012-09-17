068d38dfbc260c24278b9ed388b1e92f9a6a7eec
Howl王 <mimosa@shareday.com>
2012-09-17 09:06:05 +0800

    清理

--- models/taobao/report/daily.rb
+++
@@ -1,4 +0,0 @@
-# -*- encoding: utf-8 -*-
-
-class Taobao::Daily < Taobao::Report # 日报表  
-end
--- models/taobao/report/oversold_daily.rb
+++
@@ -1,4 +0,0 @@
-# -*- encoding: utf-8 -*-
-
-class Taobao::OversoldDaily < Taobao::Report # 日报表（超卖）
-end
